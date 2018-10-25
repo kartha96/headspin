4090a209903f92be91b8c74ce57cdcbd6b97d880
kartha96 <44439820+kartha96@users.noreply.github.com>
2018-10-25 18:25:48 +0530

    Update array.c

--- array.c
+++ array.c
@@ -3,30 +3,20 @@
 int main()
 {
    int array[100], p, i, n, num;
- 
    printf("Enter number of elements in array\n");
    scanf("%d", &n);
- 
    printf("Enter %d elements\n", n);
- 
    for (i = 0; i < n; i++)
-      scanf("%d", &array[i]);
- 
+     scanf("%d", &array[i]);
    printf("Enter the position to enter the element\n");
    scanf("%d", &p);
- 
    printf("Enter the number to insert\n");
    scanf("%d", &num);
- 
    for (i = n - 1; i >= p - 1; i--)
       array[i+1] = array[i];
- 
    array[p-1] = num;
- 
-   printf("Resultant array is\n");
- 
+   printf("Final array is\n");
    for (i = 0; i <=n; i++)
       printf("%d\n", array[i]);
- 
-   return 0;
+  return 0;
 }
